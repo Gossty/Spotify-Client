16fe4853e7f57d7821b28853fbf50e09d59f2693
Gosty <91396884+Gossty@users.noreply.github.com>
2022-07-25 22:20:38 -0700

    heroku

--- ml_spotify/settings.py
+++ ml_spotify/settings.py
@@ -12,11 +12,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv, find_dotenv
 import sys
 import dj_database_url
 
-load_dotenv(find_dotenv())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
